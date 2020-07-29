4acfb1e618934e9750dc284959872971184672ab
Baraffe Robin <robin.baraffe@gmail.com>
2020-07-29 10:10:15 +0200

    Init api (#1)

---
+++ api/api.py
@@ -0,0 +1,30 @@
+"""
+This module represent transports api methods
+"""
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+# Return transports for a given id or all transports datasets
+# Param name is city
+@app.route('/api/v1/transports', methods=['GET'])
+def get_transports():
+    """ Get all transports or transports
+    from a city
+    Param name is city
+    """
+    # Default url that return all datasets
+    url = 'https://tr.transport.data.gouv.fr'
+
+    # If request contain a city param, change url to fetch specified dataset
+    if 'city' in request.args:
+        city = request.args['city']
+        url = '%s/%s/gtfs-rt.json' % (url, city)
+
+    response = requests.get(url)
+    return response.json()
+
+
+app.run()
