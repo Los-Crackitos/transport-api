af7e6b44aecf82187520dfed0dfd0ae9b472c104
Thyrael <robin.baraffe@gmail.com>
2020-08-10 10:25:21 +0200

    Create model for /<city> route

--- apis/transport.py
+++ apis/transport.py
@@ -2,8 +2,16 @@ from flask import jsonify
 from flask_restx import Namespace, Resource, fields
 import requests
 
+
 api = Namespace('transports', description='')
 
+type_details = api.model('Type_details', { ## Modele qui est retourné par la route via le tag @api.marshal_with
+    'id': fields.Integer,
+    'isDeleted': fields.Boolean,
+    'vehicule': fields.String,
+    'alert': fields.String,
+    'line': fields.String(attribute='entity.0.tripUpdate.trip.routeId')
+})
 
 @api.route('/')
 class CitiesTransportsList(Resource):
@@ -21,10 +29,15 @@ class CitiesTransportsList(Resource):
 class CityTransportsList(Resource):
 
     @api.doc('get_transport_type')
+    # Quand @api.marshal_with est déclaré, c'est l'objet qui est retourné même si les valeurs sont nulles
+    #@api.marshal_with(type_details, envelope='transports_type_details')
     def get(self, city):
         """Fetch all transport type from a given city"""
         url = 'https://tr.transport.data.gouv.fr/%s/gtfs-rt.json' % (city)
         response = requests.get(url)
         if response.status_code == 404:
             api.abort(404)
+
+        test = response.json()
+        #return test["entity"]
         return jsonify(response.json())
