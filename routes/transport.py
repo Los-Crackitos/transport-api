740a941339f2a220d3e66bd8376d09b6818deb4d
Jérôme Pogeant <p-jerome@hotmail.fr>
2020-09-28 21:31:03 +0200

    Find transport data by city and format it (#6)

--- routes/transport.py
+++ routes/transport.py
@@ -15,8 +15,27 @@ class CitiesTransportsList(Resource):
         return jsonify(response.json())
 
 
+transport = api.schema_model(
+    'transport', {
+        'bus': [{
+            'id': fields.Integer,
+            'trip_id': fields.String,
+            'vehicle_id': fields.Integer,
+            'position': {
+                'latitude': fields.Integer,
+                'longitude': fields.Integer,
+            },
+            'speed': fields.String,
+        }],
+        'metro': [{
+            'id': fields.Integer,
+        }],
+    })
+
+
 @api.route('/<city>')
 @api.param('city', 'City of transport data')
+@api.response(200, transport)
 @api.response(404, 'City not found')
 class CityTransportsList(Resource):
 
@@ -27,4 +46,35 @@ class CityTransportsList(Resource):
         response = requests.get(url)
         if response.status_code == 404:
             api.abort(404)
-        return jsonify(response.json())
+
+        transportList = {}
+        busList = []
+        metroList = []
+        for entity in response.json()['entity']:
+            if entity['vehicle'] is not None:
+                busList.append({
+                    'id':
+                        entity['id'],
+                    'trip_id':
+                        entity['vehicle']['trip']['tripId'],
+                    'vehicle_id':
+                        entity['vehicle']['vehicle']['id'],
+                    'position': {
+                        'latitude':
+                            entity['vehicle']['position']['latitude']
+                            if entity['vehicle']['position'] is not None else
+                            'N/A',
+                        'longitude':
+                            entity['vehicle']['position']['longitude']
+                            if entity['vehicle']['position'] is not None else
+                            'N/A',
+                    },
+                    'speed':
+                        entity['vehicle']['position']['speed']
+                        if entity['vehicle']['position'] is not None else 0
+                })
+
+        transportList['bus'] = busList
+        transportList['metro'] = metroList
+
+        return transportList
