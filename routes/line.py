1376a365aeff96bf93fcb93829855905791e2fc5
Thyrael <robin.baraffe@gmail.com>
2020-09-30 16:11:59 +0200

    Create line namespace

---
+++ routes/line.py
@@ -0,0 +1,69 @@
+from flask import jsonify
+from flask_restx import Namespace, Resource, fields
+import requests
+
+api = Namespace('lines', description='')
+
+line = api.schema_model(
+    'line', {
+        'id': fields.String,
+        'stopPoint': [{
+            'pointName': fields.String,
+            'pointsCoordinates': {
+                'longitude': fields.Float,
+                'latitude': fields.Float
+            }
+        }]
+    }
+)
+
+
+@api.route('/<city>/<bus>')
+@api.param('bus', 'Bus reference id')
+#@api.response(200, line)
+@api.response(404, 'Bus not found')
+class TransportLine(Resource):
+
+    @api.doc('get_transport_line')
+    def get(self, city, bus):
+        """Fetch all transport type from a given city"""
+        url = 'https://tr.transport.data.gouv.fr/%s/gtfs-rt.json' % (city)
+        response = requests.get(url)
+    
+        if response.status_code == 404:
+            api.abort(404)
+
+        busStopIdList = []
+        for entity in response.json()['entity']:
+            if entity['tripUpdate'] is not None:
+                if entity['tripUpdate']['vehicle']['id'] == bus:
+                    for stopIds in entity['tripUpdate']['stopTimeUpdate']:
+                        busStopIdList.append(stopIds['stopId'])
+
+
+        busStopIdsNamesList = []
+        for busStopId in busStopIdList:
+            url = 'https://tr.transport.data.gouv.fr/%s/siri/2.0/stop-monitoring.json?MonitoringRef=%s' % (city, busStopId)
+            response = requests.get(url)
+            
+            if response.status_code == 404:
+                api.abort(404)
+
+            for siri in response.json()['Siri']['ServiceDelivery']['StopMonitoringDelivery']:
+                for monitoredStopVisit in siri['MonitoredStopVisit']:
+                    busStopIdsNamesList.append(monitoredStopVisit['MonitoredVehicleJourney']['MonitoredCall']['StopPointName'])
+
+        busLineStopCoordinates = []
+        for busStopName in busStopIdsNamesList: 
+            url = 'https://tr.transport.data.gouv.fr/%s/siri/2.0/stoppoints-discovery.json?q=%s' % (city, busStopName)
+            response = requests.get(url)
+
+            if response.status_code == 404:
+                api.abort(404)
+
+            busLineStopCoordinates.append({
+                "longitude": response.json()['Siri']['StopPointsDelivery']['AnnotatedStopPoint'][0]['Location']['longitude'],
+                "latitude": response.json()['Siri']['StopPointsDelivery']['AnnotatedStopPoint'][0]['Location']['latitude']
+            })
+            
+        return busLineStopCoordinates
